f5961b78ad80844a42592d2f69eddfab27ce7d5c
msarosh1 <54763184+msarosh1@users.noreply.github.com>
2020-12-22 00:55:42 +0500

    Version 1.2, Tabulation added, map tab glitches removed, direct current location will be added by qira.

--- app/src/main/java/com/example/tracker/MainActivity.java
+++ app/src/main/java/com/example/tracker/MainActivity.java
@@ -20,31 +20,32 @@ public class MainActivity extends AppCompatActivity {
         //I added this if statement to keep the selected fragment when rotating the device
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
-                    new map_frag()).commit();
+                    new MapsActivity()).commit();
         }
     }
     private BottomNavigationView.OnNavigationItemSelectedListener navListener =
-            new BottomNavigationView.OnNavigationItemSelectedListener() {
-                @SuppressLint("NonConstantResourceId")
-                @Override
-                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                    Fragment selectedFragment = null;
-                    switch (item.getItemId()) {
-                        case R.id.tab_map_item:
-                            selectedFragment = new map_frag();
-                            break;
-                        case R.id.tab_emergency_cont_item:
-                            selectedFragment = new emergency_conts();
-                            break;
-                        case R.id.tab_directory_item:
-                            selectedFragment = new directory();
-                            break;
-                        case R.id.tab_sos_item:
-                            selectedFragment = new sos();
-                    }
-                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
-                            selectedFragment).commit();
-                    return true;
+        new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @SuppressLint("NonConstantResourceId")
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment selectedFragment = null;
+                switch (item.getItemId()) {
+                    case R.id.tab_map_item:
+                        selectedFragment = new MapsActivity();
+                        break;
+                    case R.id.tab_emergency_cont_item:
+                        selectedFragment = new emergency_conts();
+                        break;
+                    case R.id.tab_directory_item:
+                        selectedFragment = new directory();
+                        break;
+                    case R.id.tab_sos_item:
+                        selectedFragment = new sos();
+                        break;
                 }
-            };
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        selectedFragment).commit();
+                return true;
+            }
+        };
 }
--- app/src/main/java/com/example/tracker/MapsActivity.java
+++ app/src/main/java/com/example/tracker/MapsActivity.java
@@ -4,33 +4,43 @@ import android.Manifest;
 import android.app.PendingIntent;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.location.Location;
-import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.telephony.SmsManager;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.core.app.ActivityCompat;
-import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.Fragment;
 
+import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.firebase.auth.FirebaseAuth;
 
-public class MapsActivity<currentView> extends FragmentActivity implements OnMapReadyCallback {
+import java.util.Objects;
 
-    private GoogleMap mMap;
+
+public class MapsActivity extends Fragment {
+
+    MapView mMapView;
+    Button logout;
+    Button zoomIn;
+    Button zoomOut;
+    Button sms;
+    private GoogleMap googleMap;
     private LocationListener locationListener;
     /**
      * checks if location is changed or not
@@ -55,107 +65,106 @@ public class MapsActivity<currentView> extends FragmentActivity implements OnMap
     String SMS_SENT = "SMS_SENT";
     String SMS_DELIVERED = "SMS_DELIVERED";
 
-    PendingIntent sentPendingIntent;
-    PendingIntent deliveredPendingIntent;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_maps);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.activity_maps, container, false);
 
-        /**
-         * checking if permissions have been granted
-         */
-        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, PackageManager.PERMISSION_GRANTED);
-        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, PackageManager.PERMISSION_GRANTED);
-        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.SEND_SMS}, 1);
+        PendingIntent sentPendingIntent;
+        PendingIntent deliveredPendingIntent;
 
-
-        sentPendingIntent = PendingIntent.getBroadcast(this, 0, new Intent(SMS_SENT), 0);
-        deliveredPendingIntent = PendingIntent.getBroadcast(this, 0, new Intent(SMS_DELIVERED), 0);
-    }
-
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
-
-        locationListener = new LocationListener() {
+        mMapView = (MapView) rootView.findViewById(R.id.map);
+        logout = (Button) rootView.findViewById(R.id.button1);
+        zoomIn = (Button) rootView.findViewById(R.id.button2);
+        zoomOut = (Button) rootView.findViewById(R.id.button3);
+        sms = (Button) rootView.findViewById(R.id.button4);
+        logout.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onLocationChanged(@NonNull Location location) {
-                try {
-                    // Add a marker in Sydney and move the camera
-                    double loc = location.getLatitude();
-                    double loc1 = location.getLongitude();
-                    LatLng nloc = new LatLng(loc, loc1);
-
-                    if (marker != null) {
-                        marker.remove();
-                    }
-
-                    marker = mMap.addMarker(new MarkerOptions().position(nloc).title("Marker in Pakistan").icon(BitmapDescriptorFactory.defaultMarker()));
-                    marker.showInfoWindow();
-                    moveToCurrentLocation(nloc, mMap);
-                } catch (SecurityException e) {
-                    e.printStackTrace();
-                }
-
-
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                Intent i = new Intent(getActivity(), SignUpActivity.class);
+                Objects.requireNonNull(getActivity()).startActivity(i);
             }
-        };
+        });
+        zoomIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                googleMap.animateCamera(CameraUpdateFactory.zoomIn());
+            }
+        });
+        zoomOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                googleMap.animateCamera(CameraUpdateFactory.zoomOut());
+            }
+        });
+        sms.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    if (ActivityCompat.checkSelfPermission(Objects.requireNonNull(getActivity()), Manifest.permission.SEND_SMS) == PackageManager.PERMISSION_GRANTED) {
+                        SendSMS();
+                    }
+                } else {
+                    requestPermissions(new String[]{Manifest.permission.SEND_SMS}, 1);
+                }
+            }
+        });
+        mMapView.onCreate(savedInstanceState);
 
-        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        mMapView.onResume(); // needed to get the map to display immediately
 
-
-        try { /** check if permissions have been granted */
-            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, Min_time, Min_distance, locationListener);
-        } catch (SecurityException e) {
+        try {
+            MapsInitializer.initialize(Objects.requireNonNull(getActivity()).getApplicationContext());
+        } catch (Exception e) {
             e.printStackTrace();
         }
-    }
 
+        sentPendingIntent = PendingIntent.getBroadcast(getContext(), 0, new Intent(SMS_SENT), 0);
+        deliveredPendingIntent = PendingIntent.getBroadcast(getContext(), 0, new Intent(SMS_DELIVERED), 0);
 
-    public void onZoom(View view) {
-        if (view.getId() == R.id.button2) {
-            mMap.animateCamera(CameraUpdateFactory.zoomIn());
-        }
-        if (view.getId() == R.id.button3) {
-            mMap.animateCamera(CameraUpdateFactory.zoomOut());
+        mMapView.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap mMap) {
+                googleMap = mMap;
+                /**
+                 * checking if permissions have been granted
+                 */
+                ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, PackageManager.PERMISSION_GRANTED);
+                ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, PackageManager.PERMISSION_GRANTED);
+                ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.SEND_SMS}, 1);
 
-        }
-        if (view.getId() == R.id.button4) {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                if (checkSelfPermission(Manifest.permission.SEND_SMS) == PackageManager.PERMISSION_GRANTED)
-                {
-                    SendSMS();
+                if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    return;
                 }
+                googleMap.setMyLocationEnabled(true);
+                // For dropping a marker at a point on the Map
+                LatLng currlocation = new LatLng(31.433961, 74.258352);
+                googleMap.addMarker(new MarkerOptions().position(currlocation).title("Marker in Pakistan").snippet(currlocation.toString()));
+                // For zooming automatically to the location of the marker
+                CameraPosition cameraPosition = new CameraPosition.Builder().target(currlocation).zoom(12).build();
+                googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
             }
-            else{
-
-                requestPermissions(new String[] {Manifest.permission.SEND_SMS}, 1);
-            }
-
-        }
-
+        });
+        return rootView;
     }
 
-    public void onLogOut(View view) {
-        if (view.getId() == R.id.button1) {
-            FirebaseAuth.getInstance().signOut();
-            Intent i = new Intent(MapsActivity.this, SignUpActivity.class);
-            startActivity(i);
-
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    public void SendSMS() {
+        String no = "+923371424828";
+        String msg = "Whassappp";
+        try {
+            SmsManager smgr = SmsManager.getDefault();
+            smgr.sendTextMessage(no, null, msg, null, null);
+            Toast.makeText(getActivity(), "SMS Sent Successfully", Toast.LENGTH_SHORT).show();
+        } catch (Exception e) {
+            Toast.makeText(getActivity(), "SMS Failed to Send, Please try again", Toast.LENGTH_SHORT).show();
         }
 
     }
@@ -166,26 +175,29 @@ public class MapsActivity<currentView> extends FragmentActivity implements OnMap
         googleMap.animateCamera(CameraUpdateFactory.zoomIn());
         // Zoom out to zoom level 10, animating with a duration of 2 seconds.
         googleMap.animateCamera(CameraUpdateFactory.zoomTo(15), 2000, null);
-
-
+    }
+    @Override
+    public void onResume() {
+        super.onResume();
+        mMapView.onResume();
     }
 
-
-    @RequiresApi(api = Build.VERSION_CODES.M)
-    public void SendSMS() {
-        String no = "+923371424828";
-        String msg = "WASSSSUPPP";
-
-
-        try {
-            SmsManager smgr = SmsManager.getDefault();
-            smgr.sendTextMessage(no, null, msg, null, null);
-            Toast.makeText(MapsActivity.this, "SMS Sent Successfully", Toast.LENGTH_SHORT).show();
-        } catch (Exception e) {
-            Toast.makeText(MapsActivity.this, "SMS Failed to Send, Please try again", Toast.LENGTH_SHORT).show();
-        }
-
+    @Override
+    public void onPause() {
+        super.onPause();
+        mMapView.onPause();
     }
 
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mMapView.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mMapView.onLowMemory();
+    }
 }
 
