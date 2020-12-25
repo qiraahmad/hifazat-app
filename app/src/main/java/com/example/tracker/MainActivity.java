28acbc1a4dc2054c3d8c4dafb8681685021c5a7f
msarosh1 <54763184+msarosh1@users.noreply.github.com>
2020-12-26 01:49:26 +0500

    Version 1.5, I've completed the emergency contacts fragment, with complete adapter to caller fragment communication via interfacing, and fragment to activity communication via SharedViewModel. Database part remains..

--- app/src/main/java/com/example/tracker/MainActivity.java
+++ app/src/main/java/com/example/tracker/MainActivity.java
@@ -7,45 +7,72 @@ import android.view.MenuItem;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+    private SharedViewModel viewModel;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
         bottomNav.setOnNavigationItemSelectedListener(navListener);
-        //I added this if statement to keep the selected fragment when rotating the device
+        //I added this if statement to keep the map activity fragment initially.
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
                     new MapsActivity()).commit();
         }
-    }
-    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
-        new BottomNavigationView.OnNavigationItemSelectedListener() {
-            @SuppressLint("NonConstantResourceId")
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                Fragment selectedFragment = null;
-                switch (item.getItemId()) {
-                    case R.id.tab_map_item:
-                        selectedFragment = new MapsActivity();
-                        break;
-                    case R.id.tab_emergency_cont_item:
-                        selectedFragment = new emergency_conts();
-                        break;
-                    case R.id.tab_directory_item:
-                        selectedFragment = new directory();
-                        break;
-                    case R.id.tab_sos_item:
-                        selectedFragment = new sos();
-                        break;
-                }
+        viewModel = new ViewModelProvider(this).get(SharedViewModel.class);
+        viewModel.getText().observe(this, item -> {
+            if (item.equals("new_cont")) {
                 getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
-                        selectedFragment).commit();
-                return true;
+                        new ContactEditor()).commit();
             }
-        };
+            else if(item.equals("cancel")) {
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new emergency_conts()).commit();
+            }
+        });
+        viewModel.getContact().observe(this, item ->{
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    new emergency_conts(item)).commit();
+        });
+        viewModel.getEditContact1().observe(this, item ->
+        {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    new ContactEditor(item)).commit();
+        });
+        viewModel.getEditContact().observe(this, item ->{
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    new emergency_conts(item.get(0), item.get(1))).commit();
+        });
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @SuppressLint("NonConstantResourceId")
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedFragment = null;
+                    switch (item.getItemId()) {
+                        case R.id.tab_map_item:
+                            selectedFragment = new MapsActivity();
+                            break;
+                        case R.id.tab_emergency_cont_item:
+                            selectedFragment = new emergency_conts();
+                            break;
+                        case R.id.tab_directory_item:
+                            selectedFragment = new directory();
+                            break;
+                        case R.id.tab_sos_item:
+                            selectedFragment = new sos();
+                            break;
+                    }
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                            selectedFragment).commit();
+                    return true;
+                }
+            };
 }
---
+++ app/src/main/java/com/example/tracker/SharedViewModel.java
@@ -0,0 +1,38 @@
+package com.example.tracker;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import java.util.ArrayList;
+
+public class SharedViewModel extends ViewModel {
+    private MutableLiveData<String> text = new MutableLiveData<>();
+    private MutableLiveData<contact> new_contact = new MutableLiveData<>();
+    private MutableLiveData<contact> edit_contact = new MutableLiveData<>();
+    private MutableLiveData<ArrayList<contact>> edit_contacts= new MutableLiveData<>();
+
+    public void setText(String input) { text.setValue(input); }
+
+    public LiveData<String> getText() {
+        return text;
+    }
+
+    public void setContact(contact c1) {
+        new_contact.setValue(c1);
+    }
+
+    public LiveData<contact> getContact() { return new_contact; }
+
+    public void setEditContact1(contact c1) { edit_contact.setValue(c1); }
+
+    public LiveData<contact> getEditContact1() { return edit_contact; }
+
+    public void setEditContact(contact c1, contact c2) {
+        ArrayList<contact> temp1 = new ArrayList<>();
+        temp1.add(c1);
+        temp1.add(c2);
+        edit_contacts.setValue(temp1);
+    }
+    public LiveData<ArrayList<contact>> getEditContact() { return edit_contacts; }
+}
