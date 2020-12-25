299c70ab8ad6c1344d06df5110ae8029d6670185
Qira <qira.ahmad@gmail.com>
2020-12-26 02:26:50 +0500

    directory tab, volume button msg activation, msg format change, along with Sarosh's latest emergency contacts

--- app/src/main/java/com/example/tracker/MainActivity.java
+++ app/src/main/java/com/example/tracker/MainActivity.java
@@ -10,10 +10,12 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+    private SharedViewModel viewModel;
     int button_count=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,32 +28,56 @@ public class MainActivity extends AppCompatActivity {
             getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
                     new MapsActivity()).commit();
         }
+        viewModel = new ViewModelProvider(this).get(SharedViewModel.class);
+        viewModel.getText().observe(this, item -> {
+            if (item.equals("new_cont")) {
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new ContactEditor()).commit();
+            }
+            else if(item.equals("cancel")) {
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new emergency_conts()).commit();
+            }
+        });
+        viewModel.getContact().observe(this, item ->{
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    new emergency_conts(item)).commit();
+        });
+        viewModel.getEditContact1().observe(this, item ->
+        {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    new ContactEditor(item)).commit();
+        });
+        viewModel.getEditContact().observe(this, item ->{
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    new emergency_conts(item.get(0), item.get(1))).commit();
+        });
     }
     private BottomNavigationView.OnNavigationItemSelectedListener navListener =
-        new BottomNavigationView.OnNavigationItemSelectedListener() {
-            @SuppressLint("NonConstantResourceId")
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                Fragment selectedFragment = null;
-                switch (item.getItemId()) {
-                    case R.id.tab_map_item:
-                        selectedFragment = new MapsActivity();
-                        break;
-                    case R.id.tab_emergency_cont_item:
-                        selectedFragment = new emergency_conts();
-                        break;
-                    case R.id.tab_directory_item:
-                        selectedFragment = new directory();
-                        break;
-                    case R.id.tab_sos_item:
-                        selectedFragment = new sos();
-                        break;
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @SuppressLint("NonConstantResourceId")
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedFragment = null;
+                    switch (item.getItemId()) {
+                        case R.id.tab_map_item:
+                            selectedFragment = new MapsActivity();
+                            break;
+                        case R.id.tab_emergency_cont_item:
+                            selectedFragment = new emergency_conts();
+                            break;
+                        case R.id.tab_directory_item:
+                            selectedFragment = new directory();
+                            break;
+                        case R.id.tab_sos_item:
+                            selectedFragment = new sos();
+                            break;
+                    }
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                            selectedFragment).commit();
+                    return true;
                 }
-                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
-                        selectedFragment).commit();
-                return true;
-            }
-        };
+            };
 
     @Override
     public boolean dispatchKeyEvent(KeyEvent event) {
